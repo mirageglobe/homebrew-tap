c6a84db4b508a56cce3b0b1f12ab7c42ebdfac98
Jimmy Lim <mirageglobe@gmail.com>
2026-05-10 15:11:03 +0100

    feat: update scout formula to v0.7.0

--- Formula/scout.rb
+++ Formula/scout.rb
@@ -4,35 +4,35 @@
 class Scout < Formula
   desc "terminal-native dual-pane file explorer"
   homepage "https://github.com/mirageglobe/scout"
-  version "0.6.0"
-  license "MIT"
+  version "0.7.0"
+  license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mirageglobe/scout/releases/download/v0.6.0/scout-darwin-amd64"
-      sha256 "707373f8bf8a38154967562c30f1558264e07ecbb65086bf4d5b924f0b545275"
+      url "https://github.com/mirageglobe/scout/releases/download/v0.7.0/scout_0.7.0_darwin_amd64.tar.gz"
+      sha256 "bc999974d18b9908930e727f0ebb359729bae6c33f4ce69c9603704e9193de21"
 
       def install
-        bin.install "scout-darwin-amd64" => "scout"
+        bin.install "scout"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/mirageglobe/scout/releases/download/v0.6.0/scout-darwin-arm64"
-      sha256 "637ac113bbb700553e9893d2fd364d64f7726620c89b41358f75e0f8342ec644"
+      url "https://github.com/mirageglobe/scout/releases/download/v0.7.0/scout_0.7.0_darwin_arm64.tar.gz"
+      sha256 "236cfa8f56ac572c2ff9c6652d4a9d6afb176f2453b2830adac39d29583e2450"
 
       def install
-        bin.install "scout-darwin-arm64" => "scout"
+        bin.install "scout"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mirageglobe/scout/releases/download/v0.6.0/scout-linux-amd64"
-      sha256 "53be44c3873fdc3431ce43634b9b09e5dac4d9eb2ea5290b57b05abfb0302bfb"
+      url "https://github.com/mirageglobe/scout/releases/download/v0.7.0/scout_0.7.0_linux_amd64.tar.gz"
+      sha256 "8a9a47477e8ab718bee57d33adcd5f667732cf43eaa38525d4417260b9b25e5e"
 
       def install
-        bin.install "scout-linux-amd64" => "scout"
+        bin.install "scout"
       end
     end
   end
